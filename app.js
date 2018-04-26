f56949c8d37a73da520d3fec997574ee1a726a32
Jake Overall <joverall22@gmail.com>
2018-04-26 11:59:35 -0600

    Ramsey

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-var cats = [{
-  img : "//placehold.it/200x100",
+var cat = {
+  img : "https://img.etsystatic.com/il/37657c/637638639/il_fullxfull.637638639_32sw.jpg?version=0",
   name : "Mr. Snibbly",
   mood : ["happy", "bitey", "shanky"],
   tolerance : 3,
   pets : 0,
   moodIndex : 0
-}]
+}
 
-function drawCat(arr) {
+function drawCat() {
   var template = ''
   
-} 
+    template += `
+    <div class="col">
+        <div class="card">
+          <img src="${cat.img}" alt="">
+          <div class="card-body">
+            <h4>name: ${cat.name}</h4>
+            <h4>happiness: ${cat.mood[cat.moodIndex]}</h4>
+            <button onclick="petCat()">Pet</button>
+          </div>
+        </div>
+      </div>
+    `
+    document.getElementById('cat').innerHTML = template
+}
+drawCat()

ecd3a2bfa5eb59bc885e683f6aa71602545ba732
Jake Overall <joverall22@gmail.com>
2018-04-26 11:47:56 -0600

    Samuel

---
+++ app.js
@@ -0,0 +1,13 @@
+var cats = [{
+  img : "//placehold.it/200x100",
+  name : "Mr. Snibbly",
+  mood : ["happy", "bitey", "shanky"],
+  tolerance : 3,
+  pets : 0,
+  moodIndex : 0
+}]
+
+function drawCat(arr) {
+  var template = ''
+  
+} 
